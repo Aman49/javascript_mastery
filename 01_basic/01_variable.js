c6ea7c37f95a98cfbc63f8cf1bb64f4cb0826dce
Aman49 <50906027+Aman49@users.noreply.github.com>
2023-10-19 18:02:30 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+var accountId = "1234"
+let city = "Delhi"
+const country = "America"
+name = "Aman"
+// assignment to constant variable is not possible country = "India"
+accountId = "126"
+let salary;
+console.log(accountId);
+console.table([accountId, city, country, name, salary])
+/* 
+prefer not to use var because of issue in block scope and functional scope
+*/
